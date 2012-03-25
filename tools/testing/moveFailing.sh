865844411be0e5c3bfacd355bfb4ea0fdfff07f4
Chucky Ellison <celliso2@illinois.edu>
2012-03-25 01:22:59 -0500

    adding warning comment to moveFailing

--- tools/testing/moveFailing.sh
+++ tools/testing/moveFailing.sh
@@ -12,4 +12,5 @@ script="\.\.\/tools\/testing\/fails\.sh"
 grep 'out\.\.\. failed' $log | sed 's/\.out\.\.\. .*//' | sed 's/\/results//' | sed "s/^/$script /" > $log.tmp
 grep 'out\.\.\. erred' $log | sed 's/\.out\.\.\. .*//' | sed 's/\/results//' | sed "s/^/$script /" > $log.tmp
 cat $log.tmp
+# this script is untested
 # bash -x $log.tmp

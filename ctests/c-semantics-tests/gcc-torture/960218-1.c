7c287710f0b6ccc53395d5e53042b2ea1ca0275c
qangel <liyili2@illinois.edu>
2015-01-10 20:14:31 -0600

    adding all the c tests which are images of the llvm tests in the tests directory

---
+++ ctests/c-semantics-tests/gcc-torture/960218-1.c
@@ -0,0 +1,26 @@
+void exit(int status);
+void abort(void);
+int glob;
+
+int g (x)
+      int x;
+{
+  glob = x;
+  return 0;
+}
+
+void f (x)
+      int x;
+{
+  int a = ~x;
+  while (a)
+    a = g (a);
+}
+
+int main ()
+{
+  f (3);
+  if (glob != -4)
+    abort ();
+  exit (0);
+}
